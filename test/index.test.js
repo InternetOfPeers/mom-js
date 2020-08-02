6d0a0c04b6b1c226dafd23ca742fb6d19fc64bcc
Giuseppe Bertone <bertone.giuseppe@gmail.com>
2020-08-02 19:00:23 +0200

    Added "ADD & REFER" command

--- test/index.test.js
+++ test/index.test.js
@@ -22,10 +22,21 @@ describe("MOM", function() {
 			expect(() => mom.createAddTransaction(expectedAddress, Buffer.from("test"))).to.throw();
 		});
 
+		it('should create a correct ADD & REFER message payload', function() {
+			const messageHash = "QmbHQieckNGj2KwBhpzkGSLDgezGnArL6eeuvb87YLX665";
+			const messageMultiHash = multihashes.fromB58String(messageHash);
+			const address = "0xF22E75d6d61D33aE38f8360574A8b72816176D20";
+			const expectedData = Buffer.concat([Buffer.from([1]), messageMultiHash, Buffer.from(address)]);
+			const expectedPayload = { to: expectedAddress, value: 0, data: expectedData };
+			mom.createAddAndReferTransaction(expectedAddress, messageMultiHash, address).should.jsonEqual(expectedPayload);
+			expect(() => mom.createAddAndReferTransaction(expectedAddress, Buffer.from("test"), address)).to.throw();
+			expect(() => mom.createAddAndReferTransaction(expectedAddress, messageMultiHash, "test")).to.throw();
+		});
+
 		it('should create a correct DELETE message payload', function() {
 			const messageHash = "QmbHQieckNGj2KwBhpzkGSLDgezGnArL6eeuvb87YLX665";
 			const messageMultiHash = multihashes.fromB58String(messageHash);
-			const expectedData = Buffer.concat([Buffer.from([1]), messageMultiHash]);
+			const expectedData = Buffer.concat([Buffer.from([2]), messageMultiHash]);
 			const expectedPayload = { to: expectedAddress, value: 0, data: expectedData };
 			mom.createDeleteTransaction(expectedAddress, messageMultiHash).should.jsonEqual(expectedPayload);
 			expect(() => mom.createDeleteTransaction(expectedAddress, Buffer.from("test"))).to.throw();
@@ -36,10 +47,11 @@ describe("MOM", function() {
 			const originalMessageMultiHash = multihashes.fromB58String(originalMessageHash);
 			const updatedMessageHash = "QmQpHt3EoEppWrVt2tAV22VFKTZ9NrMwfAWSsGcBZv6WKw";
 			const updatedMessageMultiHash = multihashes.fromB58String(updatedMessageHash);
-			const expectedData = Buffer.concat([Buffer.from([2]), originalMessageMultiHash, updatedMessageMultiHash]);
+			const expectedData = Buffer.concat([Buffer.from([3]), originalMessageMultiHash, updatedMessageMultiHash]);
 			const expectedPayload = { to: expectedAddress, value: 0, data: expectedData };
 			mom.createUpdateTransaction(expectedAddress, originalMessageMultiHash, updatedMessageMultiHash).should.jsonEqual(expectedPayload);
-			expect(() => mom.createUpdateTransaction(expectedAddress, Buffer.from("test"), Buffer.from("test"))).to.throw();
+			expect(() => mom.createUpdateTransaction(expectedAddress, Buffer.from("test"), updatedMessageMultiHash)).to.throw();
+			expect(() => mom.createUpdateTransaction(expectedAddress, originalMessageMultiHash, Buffer.from("test"))).to.throw();
 		});
 
 		it('should create a correct REPLY message payload', function() {
@@ -47,16 +59,17 @@ describe("MOM", function() {
 			const originalMessageMultiHash = multihashes.fromB58String(originalMessageHash);
 			const replyMessageHash = "QmQpHt3EoEppWrVt2tAV22VFKTZ9NrMwfAWSsGcBZv6WKw";
 			const replyMessageMultiHash = multihashes.fromB58String(replyMessageHash);
-			const expectedData = Buffer.concat([Buffer.from([3]), originalMessageMultiHash, replyMessageMultiHash]);
+			const expectedData = Buffer.concat([Buffer.from([4]), originalMessageMultiHash, replyMessageMultiHash]);
 			const expectedPayload = { to: expectedAddress, value: 0, data: expectedData };
 			mom.createReplyTransaction(expectedAddress, originalMessageMultiHash, replyMessageMultiHash).should.jsonEqual(expectedPayload);
-			expect(() => mom.createReplyTransaction(expectedAddress, Buffer.from("test"), Buffer.from("test"))).to.throw();
+			expect(() => mom.createReplyTransaction(expectedAddress, Buffer.from("test"), replyMessageMultiHash)).to.throw();
+			expect(() => mom.createReplyTransaction(expectedAddress, originalMessageMultiHash, Buffer.from("test"))).to.throw();
 		});
 
 		it('should create a correct ENDORSE message payload', function() {
 			const messageHash = "QmbHQieckNGj2KwBhpzkGSLDgezGnArL6eeuvb87YLX665";
 			const messageMultiHash = multihashes.fromB58String(messageHash);
-			const expectedData = Buffer.concat([Buffer.from([4]), messageMultiHash]);
+			const expectedData = Buffer.concat([Buffer.from([5]), messageMultiHash]);
 			const expectedPayload = { to: expectedAddress, value: 0, data: expectedData };
 			mom.createEndorseTransaction(expectedAddress, messageMultiHash).should.jsonEqual(expectedPayload);
 			expect(() => mom.createEndorseTransaction(expectedAddress, Buffer.from("test"))).to.throw();
@@ -65,7 +78,7 @@ describe("MOM", function() {
 		it('should create a correct DISAPPROVE message payload', function() {
 			const messageHash = "QmbHQieckNGj2KwBhpzkGSLDgezGnArL6eeuvb87YLX665";
 			const messageMultiHash = multihashes.fromB58String(messageHash);
-			const expectedData = Buffer.concat([Buffer.from([5]), messageMultiHash]);
+			const expectedData = Buffer.concat([Buffer.from([6]), messageMultiHash]);
 			const expectedPayload = { to: expectedAddress, value: 0, data: expectedData };
 			mom.createDisapproveTransaction(expectedAddress, messageMultiHash).should.jsonEqual(expectedPayload);
 			expect(() => mom.createDisapproveTransaction(expectedAddress, Buffer.from("test"))).to.throw();
@@ -76,10 +89,11 @@ describe("MOM", function() {
 			const originalMessageMultiHash = multihashes.fromB58String(originalMessageHash);
 			const replyMessageHash = "QmQpHt3EoEppWrVt2tAV22VFKTZ9NrMwfAWSsGcBZv6WKw";
 			const replyMessageMultiHash = multihashes.fromB58String(replyMessageHash);
-			const expectedData = Buffer.concat([Buffer.from([6]), originalMessageMultiHash, replyMessageMultiHash]);
+			const expectedData = Buffer.concat([Buffer.from([7]), originalMessageMultiHash, replyMessageMultiHash]);
 			const expectedPayload = { to: expectedAddress, value: 0, data: expectedData };
 			mom.createEndorseAndReplyTransaction(expectedAddress, originalMessageMultiHash, replyMessageMultiHash).should.jsonEqual(expectedPayload);
-			expect(() => mom.createEndorseAndReplyTransaction(expectedAddress, Buffer.from("test"), Buffer.from("test"))).to.throw();
+			expect(() => mom.createEndorseAndReplyTransaction(expectedAddress, Buffer.from("test"), replyMessageMultiHash)).to.throw();
+			expect(() => mom.createEndorseAndReplyTransaction(expectedAddress, originalMessageMultiHash, Buffer.from("test"))).to.throw();
 		});
 
 		it('should create a correct DISAPPROVE & REPLY message payload', function() {
@@ -87,10 +101,11 @@ describe("MOM", function() {
 			const originalMessageMultiHash = multihashes.fromB58String(originalMessageHash);
 			const replyMessageHash = "QmQpHt3EoEppWrVt2tAV22VFKTZ9NrMwfAWSsGcBZv6WKw";
 			const replyMessageMultiHash = multihashes.fromB58String(replyMessageHash);
-			const expectedData = Buffer.concat([Buffer.from([7]), originalMessageMultiHash, replyMessageMultiHash]);
+			const expectedData = Buffer.concat([Buffer.from([8]), originalMessageMultiHash, replyMessageMultiHash]);
 			const expectedPayload = { to: expectedAddress, value: 0, data: expectedData };
 			mom.createDisapproveAndReplyTransaction(expectedAddress, originalMessageMultiHash, replyMessageMultiHash).should.jsonEqual(expectedPayload);
-			expect(() => mom.createDisapproveAndReplyTransaction(expectedAddress, Buffer.from("test"), Buffer.from("test"))).to.throw();
+			expect(() => mom.createDisapproveAndReplyTransaction(expectedAddress, Buffer.from("test"), replyMessageMultiHash)).to.throw();
+			expect(() => mom.createDisapproveAndReplyTransaction(expectedAddress, originalMessageMultiHash, Buffer.from("test"))).to.throw();
 		});
 
 		it('should create a correct CLOSE ACCOUNT message payload', function() {
